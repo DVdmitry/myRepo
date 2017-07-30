25a432f932cdc36b8d88a689c4edc9148134c686
Dmitry <isaevski.dv@gmail.com>
2017-07-30 07:47:58 +0300

    gitignore commit

---
+++ Events/Events/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    //class Person
+    //{
+    //    public delegate void EventHandler();
+    //    public event EventHandler cashEvent;
+    //    private int _cash;
+    //    public int Cash
+    //    {
+    //        set
+    //        {
+    //            if (value >= 0)
+    //                this._cash = value;
+    //            if (this._cash >= 100 && cashEvent != null)
+    //                cashEvent();
+    //        }
+    //        get
+    //        {
+    //            return this._cash;
+    //        }
+    //    }
+
+    //    public void AddCash(int amount)
+    //    {
+    //        Cash += amount;
+    //    }
+    //}
+}
---
+++ Events/Events/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Events
+{
+    class Program
+    {
+        public delegate bool LessNum(int num);
+
+        public static bool GetLessFive(int num)
+        {
+            return (num < 5); 
+        }
+
+        public static bool GetGreater20(int num)
+        {
+            return (num > 20);
+        }
+
+        
+        public static IEnumerable<int> GetNums(IEnumerable<int> array, LessNum meDel)
+        {
+            foreach (int item in array)
+            {
+                meDel(item);
+                yield return item;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = new[] { 2, 6, 12, 67, 2, 5, 6, 99 };
+            LessNum md = Program.GetLessFive;
+            // md += Program.GetGreater20;
+            foreach(int item in Program.GetNums(arr, md))
+                Console.WriteLine(item);
+           
+            Console.ReadKey();
+        }
+
+    }
+}
